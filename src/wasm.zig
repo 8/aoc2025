230fd3b4b0bb4b200c4ba12c56dbb486a1bddb81
mdk <mdk@lostindetails.com>
2025-12-03 07:28:44 +0100

    updates wasm

--- src/wasm.zig
+++ src/wasm.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const day1 = @import("day01.zig");
 const day2 = @import("day02.zig");
+const day3 = @import("day03.zig");
 
 export fn day1_part1() i64 {
   return day1.part1() catch -1;
@@ -13,4 +14,7 @@ export fn day2_part1() i64 {
 }
 export fn day2_part2() i64 {
   return day2.part2() catch -1;
+}
+export fn day3_part1() i64 {
+  return day3.part1() catch -1;
 }
